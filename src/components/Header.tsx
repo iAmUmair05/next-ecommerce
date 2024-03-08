ec53684404c57736ce1eed9fa8ebead623396cad
Umair Younus <Hamadumair33@gmail.com>
2024-03-08 16:55:47 +0500

    header updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,18 +49,11 @@ const Header = () => {
 
   return (
     <div className="bg-cyan-400 h-20 top-0 sticky z-50">
-      <Container className="h-full flex items-center md:gap-x-3 justify-between md:justify-start">
+      <Container className="h-full flex items-center md:gap-x-3 justify-between">
         <Logo />
         {/* Search Field */}
 
-        <div className="w-full bg-white hidden md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-cyan-400 group">
-          <FiSearch className="text-gray-300 group-focus-within:text-cyan-400 duration-200" />
-          <input
-            type="text"
-            placeholder="Search for products"
-            className="placeholder:text-sm flex-1 outline-none"
-          />
-        </div>
+
 
         {/* favorite button */}
         {/* 
@@ -71,58 +64,70 @@ const Header = () => {
 
         {/* Login/Register */}
 
-        {!session && (
-          <div onClick={() => signIn()} className="headerDiv cursor-pointer">
-            <AiOutlineUser className="text-2xl" />
-            <p className="text-sm font-semibold">Login/Register</p>
-          </div>
-        )}
-        {/* Cart button */}
+        <div className="flex items-center gap-2">
 
-        <Link href={"/cart"}>
-          <div className="bg-black hover:bg-slate-950 rounded-full text-slate-100 hover:text-white flex items-center justify-center gap-x-1 px-3 py-1.5 border-[1px] border-black hover:border-cyan-600 duration-200 relative">
-            <IoMdCart className="text-xl" />
-            {/* <p className="text-sm font-semibold">
+          {!session && (
+            <div onClick={() => signIn()} className="headerDiv cursor-pointer">
+              <AiOutlineUser className="text-2xl" />
+              <p className="text-sm font-semibold">Login/Register</p>
+            </div>
+          )}
+
+
+          {/* Cart button */}
+
+          <Link href={"/cart"}>
+            <div className="bg-black hover:bg-slate-950 rounded-full text-slate-100 hover:text-white flex items-center justify-center gap-x-1 px-3 py-1.5 border-[1px] border-black hover:border-cyan-600 duration-200 relative">
+              <IoMdCart className="text-xl" />
+              {/* <p className="text-sm font-semibold">
               <FormattedPrice amount={totalAmt ? totalAmt : 0} />
             </p> */}
-            <span className="bg-white text-cyan-600 rounded-full text-xs font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center shadow-xl shadow-black">
-              {productData ? productData?.length : 0}
-            </span>
-          </div>
-        </Link>
-        {/* user Image */}
-
-        {session && (
-          <Image
-            src={session?.user?.image as string}
-            alt="user image"
-            width={50}
-            height={50}
-            className="rounded-full object-cover"
-          />
-        )}
-
-        {/* Order button */}
-        {orderData?.order?.length > 0 && session && (
-          <Link
-            href={"/order"}
-            className="headerDiv px-2 gap-x-1 cursor-pointer"
-          >
-            <BsBookmarks className="text-2xl" />
-            <p className="text-sm font-semibold">Orders</p>
+              <span className="bg-white text-cyan-600 rounded-full text-xs font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center shadow-xl shadow-black">
+                {productData ? productData?.length : 0}
+              </span>
+            </div>
           </Link>
-        )}
 
-        {/* Logout button */}
-        {session && (
-          <div
-            onClick={() => signOut()}
-            className="headerDiv px-2 gap-x-1 cursor-pointer"
-          >
-            <FiLogOut className="text-2xl" />
-            <p className="text-sm font-semibold">Logout</p>
-          </div>
-        )}
+
+
+          {/* user Image */}
+
+          {session && (
+            <Image
+              src={session?.user?.image as string}
+              alt="user image"
+              width={50}
+              height={50}
+              className="rounded-full object-cover"
+            />
+
+          )}
+
+          {/* Order button */}
+          {orderData?.order?.length > 0 && session && (
+            <Link
+              href={"/order"}
+              className="headerDiv px-2 gap-x-1 cursor-pointer"
+            >
+              <BsBookmarks className="text-2xl" />
+              <p className="text-sm font-semibold">Orders</p>
+            </Link>
+          )}
+
+
+          {/* Logout button */}
+          {session && (
+            <div
+              onClick={() => signOut()}
+              className="headerDiv px-2 gap-x-1 cursor-pointer"
+            >
+              <FiLogOut className="text-2xl" />
+              <p className="text-sm font-semibold">Logout</p>
+            </div>
+          )}
+
+        </div>
+
 
       </Container>
     </div>
